dc7d1be4530170c139a27cfa4a4e9d03a733df45
tommy-anderson <alexursu@planable.io>
2022-04-01 14:10:33 +0300

    👌

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 module.exports = ({github, context}) => {
+  const {payload} = context;
+  const {issue} = payload;
+  const {pull_request} = issue
+  console.log(JSON.stringify(issue))
     return 'Hello World!'
   }
