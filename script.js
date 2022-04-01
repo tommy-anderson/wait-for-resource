6a51855c485beda0bbd63183cd179b1c5431e689
tommy-anderson <alexursu@planable.io>
2022-04-01 14:20:18 +0300

    test

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-module.exports = ({github, context}) => {
+module.exports = ({context,token}) => {
   const {payload} = context;
   const {issue} = payload;
   const {pull_request} = issue
+  console.log(token)
   console.log(JSON.stringify(context))
-    return 'Hello World!'
+    return payload.user.id.toString()
   }
