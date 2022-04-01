09e6582b76e1480fb6734158f5a24f9b30d42c6e
tommy-anderson <alexursu@planable.io>
2022-04-01 14:27:13 +0300

    👌

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 module.exports = (context,token) => {
   const {payload} = context;
-  const {issue} = payload;
   const {pull_request} = issue
-  console.log(token)
-  console.log(JSON.stringify(context))
-    return payload.user.id.toString()
+  console.log(JSON.stringify(pull_request))
+    return payload.comments.user.id.toString()
   }
