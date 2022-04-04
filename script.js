b853ac1be9293a8b8eeb3568ae2b58b9c2213e05
tommy-anderson <alexursu@planable.io>
2022-04-04 12:52:59 +0300

    👌 Try with async

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-module.exports = (context,token) => {
+module.exports = async (context) => {
   const {payload} = context;
   const {issue} = payload
   const {pull_request} = issue
   console.log(JSON.stringify(pull_request))
-    return payload.comment.user.id.toString()
+  await waitForStuffToHappen()
+  return payload.comment.user.login.toString()
+  }
+
+  async function waitForStuffToHappen() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+      console.log('waited for stuff to happen')
+        resolve()
+      }, 10000)
+    })
   }
